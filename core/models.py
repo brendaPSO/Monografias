5dba33aa5bccf200165095a6c943bdb6a035f542
Brenda Orlandi <brenda.orlandi@ufvjm.edu.br>
2022-10-19 00:16:55 -0300

    Cadastro de pessoas

--- core/models.py
+++ core/models.py
@@ -2,7 +2,23 @@ from django.db import models
 
 
 class Pessoa(models.Model):
-    nome = models.CharField(max_length=100)
+    TIPO_PESSOAS = (
+        (1, 'Autor'),
+        (2, 'Coautor'),
+        (3, 'Discentes'),
+    )
+
+    nome = models.CharField(max_length=100, default='')
+    tipo = models.CharField(max_length=1, choices=TIPO_PESSOAS, default=1)
+    curso = models.CharField(max_length=100, default='')
+    universidade = models.CharField(max_length=128, default='')
+    email = models.EmailField(max_length=254, unique=True, default='')
+    curriculo = models.URLField(max_length=128, null=True, blank=True)
+    google_scholar = models.URLField(max_length=128, null=True, blank=True)
+    research_gate = models.URLField(max_length=128, null=True, blank=True)
+    linkedin = models.URLField(max_length=128, null=True, blank=True)
+    orcid = models.URLField(max_length=128, null=True, blank=True)
+    github = models.URLField(max_length=128, null=True, blank=True)
 
     def __str__(self):
         return self.nome
--- core/views.py
+++ core/views.py
@@ -8,9 +8,34 @@ def home(request):
 
 
 def salvar(request):
-    nome = request.POST.get('nome')
-    Pessoa.objects.create(nome=nome)
+    r_nome = request.POST.get('nome')
+    r_tipo = request.POST.get('tipo')
+    r_curso = request.POST.get('curso')
+    r_universidade = request.POST.get('universidade')
+    r_email = request.POST.get('email')
+    r_curriculo = request.POST.get('curriculo')
+    r_google_scholar = request.POST.get('google_scholar')
+    r_research_gate = request.POST.get('research_gate')
+    r_linkedin = request.POST.get('linkedin')
+    r_orcid = request.POST.get('orcid')
+    r_github = request.POST.get('github')
+
+    Pessoa.objects.create(
+        nome=r_nome,
+        tipo=r_tipo,
+        curso=r_curso,
+        universidade=r_universidade,
+        email=r_email,
+        curriculo=r_curriculo,
+        google_scholar=r_google_scholar,
+        research_gate=r_research_gate,
+        linkedin=r_linkedin,
+        orcid=r_orcid,
+        github=r_github,
+    )
+
     pessoas = Pessoa.objects.all()
+
     return render(request, 'index.html', {"pessoas": pessoas})
 
 

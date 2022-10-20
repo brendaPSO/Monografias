7a2b5f3200924daf20ceb97f72177f70735fb9ec
Guilherme Jardim Neves Pereira <Guijardimnp@hotmail.com>
2022-10-20 16:04:06 -0300

    adcionado campo no settings.py para integtrar com postgresql.

--- app/settings.py
+++ app/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'app',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +77,11 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Monografias',
+        'USER': 'postgres',
+        'PASSWORD': '07142003',
+        'HOST': 'localhost',
     }
 }
 

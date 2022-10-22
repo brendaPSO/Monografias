9c3a137fbd0a939cfa8cff14b89e9f0ccc1eac0f
Brenda Orlandi <brenda.orlandi@ufvjm.edu.br>
2022-10-22 20:03:42 -0300

    Correções - tamanho dos campos

--- core/models.py
+++ core/models.py
@@ -8,17 +8,17 @@ class Pessoa(models.Model):
         (3, 'Discentes'),
     )
 
-    nome = models.CharField(max_length=100, default='')
+    nome = models.CharField(max_length=80, default='')
     tipo = models.CharField(max_length=1, choices=TIPO_PESSOAS, default=1)
     curso = models.CharField(max_length=100, default='')
     universidade = models.CharField(max_length=128, default='')
-    email = models.EmailField(max_length=254, unique=True, default='')
-    curriculo = models.URLField(max_length=128, null=True, blank=True)
-    google_scholar = models.URLField(max_length=128, null=True, blank=True)
-    research_gate = models.URLField(max_length=128, null=True, blank=True)
-    linkedin = models.URLField(max_length=128, null=True, blank=True)
-    orcid = models.URLField(max_length=128, null=True, blank=True)
-    github = models.URLField(max_length=128, null=True, blank=True)
+    email = models.EmailField(max_length=256, unique=True, default='')
+    curriculo = models.URLField(max_length=512, null=True, blank=True)
+    google_scholar = models.URLField(max_length=512, null=True, blank=True)
+    research_gate = models.URLField(max_length=512, null=True, blank=True)
+    linkedin = models.URLField(max_length=512, null=True, blank=True)
+    orcid = models.URLField(max_length=512, null=True, blank=True)
+    github = models.URLField(max_length=512, null=True, blank=True)
 
     def __str__(self):
         return self.nome
--- monografia/models.py
+++ monografia/models.py
@@ -2,16 +2,16 @@ from django.db import models
 
 # Create your models here.
 class Monografia (models.Model):
-    titulo = models.CharField(max_length=200, default = "")
-    autor = models.CharField(max_length=100, default = "")
-    orientador = models.CharField(max_length=100, default = "")
-    coorientador = models.CharField(max_length=100, default = "")
-    data = models.CharField(max_length=50, default = "")
-    resumo = models.CharField(max_length=250, default = "")
-    palavrachave = models.CharField("Palavra-chave", max_length=200, default = "")
-    universidade = models.CharField(max_length=100, default = "")
+    titulo = models.CharField(max_length=256, default = "")
+    autor = models.CharField(max_length=80, default = "")
+    orientador = models.CharField(max_length=80, default = "")
+    coorientador = models.CharField(max_length=80, default = "")
+    data = models.DateField(max_length=30, default = "")
+    resumo = models.TextField(max_length=3200, default = "")
+    palavrachave = models.CharField("Palavra-chave", max_length=100, default = "")
+    universidade = models.CharField(max_length=128, default = "")
     curso = models.CharField(max_length=100, default = "")
-    link = models.CharField("Link para PDF", max_length=300, default = "")
+    link = models.CharField("Link para PDF", max_length=512, default = "")
 
     def __str__(self):
         return self.nome

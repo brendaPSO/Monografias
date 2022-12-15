b38c2781435b76f5d5730e67de67c6a3ddd8019c
Brenda Orlandi <brenda.orlandi@ufvjm.edu.br>
2022-12-15 16:59:08 -0300

    Correção - Pesquisar monografia e layout

--- monografia/Filters.py
+++ monografia/Filters.py
@@ -2,12 +2,13 @@ from turtle import title
 
 import django_filters
 from .models import Monografia
+from core.models import Pessoa
 
 class MonografiaFilter(django_filters.FilterSet):
     titulo = django_filters.CharFilter(lookup_expr='icontains', label='Título')
-    autor = django_filters.CharFilter(lookup_expr='icontains', label='Autor')
-    orientador = django_filters.CharFilter(lookup_expr='icontains', label='Orientador')
-    coorientador = django_filters.CharFilter(lookup_expr='icontains', label='Coorientador')
+    autor__nome = django_filters.CharFilter(lookup_expr='icontains', label='Autor')
+    orientador__nome = django_filters.CharFilter(lookup_expr='icontains', label='Orientador')
+    coorientador__nome = django_filters.CharFilter(lookup_expr='icontains', label='Coorientador')
     resumo = django_filters.CharFilter(lookup_expr='icontains', label='Resumo')
     palavrachave = django_filters.CharFilter(lookup_expr='icontains', label='Palavra-Chave')
     universidade = django_filters.CharFilter(lookup_expr='icontains', label='Universidade')
@@ -15,4 +16,4 @@ class MonografiaFilter(django_filters.FilterSet):
 
     class Meta:
         model = Monografia
-        fields = ('titulo','autor', 'orientador', 'coorientador', 'resumo', 'palavrachave', 'universidade', 'curso')
+        fields = ('titulo','autor__nome', 'orientador__nome', 'coorientador__nome', 'resumo', 'palavrachave', 'universidade', 'curso')
